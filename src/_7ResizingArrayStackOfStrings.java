1ae4d613fce22c87a448664f0fa6e1813c4d880c
Abdullah Khan <cmabdullah21@gmail.com>
2017-04-30 13:31:44 +0600

    Done Resizing Array Stack Of Strings

---
+++ src/_7ResizingArrayStackOfStrings.java
@@ -0,0 +1,41 @@
+class ResizingArraySOfS{
+	private String[] s;
+	private int N = 0;
+	public ResizingArraySOfS(){
+		s = new String[1];
+	}
+	public void push(String item){
+		if (N == s.length)
+			resize(2 * s.length);
+			s[N++] = item;
+	}
+	private void resize(int capacity){
+		String[] copy = new String[capacity];
+		for (int i = 0; i < N; i++)
+		copy[i] = s[i];
+		s = copy;
+	}
+	public String pop(){
+		String item = s[--N];
+		s[N] = null;
+		if (N > 0 && N == s.length/4){
+			resize(s.length/2);
+		}
+		return item;
+	}
+}
+public class _7ResizingArrayStackOfStrings {
+
+	public static void main(String[] args){
+		ResizingArraySOfS stack = new ResizingArraySOfS();
+		while (!StdIn.isEmpty()){
+			String s = StdIn.readString();
+			if (s.equals("-")){
+				StdOut.println("Poped : "+stack.pop());
+			}
+			else{
+				stack.push(s);
+			}
+		}
+	}
+}
