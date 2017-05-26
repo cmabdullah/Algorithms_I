155f3c7874009bf2422dbbb2a60c3ad2084b26d5
Abdullah Khan <cmabdullah21@gmail.com>
2017-05-26 11:45:01 +0600

    Ultimately done RedBlackBST

--- src/BinaryDump.java
+++ src/BinaryDump.java
@@ -22,20 +22,21 @@
 
 public class BinaryDump {
 
-    public static void main(String[] args) {
-        int BITS_PER_LINE = 16;
-        if (args.length == 1) {
-            BITS_PER_LINE = Integer.parseInt(args[0]);
-        }
+	public static void main(String[] args) {
+		int BITS_PER_LINE = 16;
+		if (args.length == 1) {
+			BITS_PER_LINE = Integer.parseInt(args[0]);
+		}
 
-        int count;
-        for (count = 0; !BinaryStdIn.isEmpty(); count++) {
-            if (BITS_PER_LINE == 0) { BinaryStdIn.readBoolean(); continue; }
-            else if (count != 0 && count % BITS_PER_LINE == 0) StdOut.println();
-            if (BinaryStdIn.readBoolean()) StdOut.print(1);
-            else                           StdOut.print(0);
-        }
-        if (BITS_PER_LINE != 0) StdOut.println();
-        StdOut.println(count + " bits");
-    }
+		int count;
+		for (count = 0; !BinaryStdIn.isEmpty(); count++) {
+			if (BITS_PER_LINE == 0) { BinaryStdIn.readBoolean(); continue; }
+			else if (count != 0 && count % BITS_PER_LINE == 0) StdOut.println();
+			if (BinaryStdIn.readBoolean()) StdOut.print(1);
+			else
+				StdOut.print(0);
+		}
+		if (BITS_PER_LINE != 0) StdOut.println();
+		StdOut.println(count + " bits");
+	}
 }
--- src/_20MaximumPriorityQueue.java
+++ src/_20MaximumPriorityQueue.java
@@ -1,250 +1,111 @@
-/*************************************************************************
- *  Compilation:  javac MaxPQ.java
- *  Execution:    java MaxPQ < input.txt
- *  
- *  Generic max priority queue implementation with a binary heap.
- *  Can be used with a comparator instead of the natural order,
- *  but the generic Key type must still be Comparable.
- *
- *  % java MaxPQ < tinyPQ.txt 
- *  Q X P (6 left on pq)
- *
- *  We use a one-based array to simplify parent and child calculations.
- *
- *  Can be optimized by replacing full exchanges with half exchanges 
- *  (ala insertion sort).
- *
- *************************************************************************/
-
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class _20MaximumPriorityQueue<Key> implements Iterable<Key> {
-	private Key[] pq;					// store items at indices 1 to N
-	private int N;						// number of items on priority queue
-	private Comparator<Key> comparator;	// optional Comparator
-
-	/**
-	 * Initializes an empty priority queue with the given initial capacity.
-	 * @param initCapacity the initial capacity of the priority queue
+public class _20MaximumPriorityQueue<Key extends Comparable<Key>>{
+	/* 
+	 * Implementation of a Max-heap
+	 * Uses lgN amortized time	
 	 */
-	public _20MaximumPriorityQueue(int initCapacity) {
-		pq = (Key[]) new Object[initCapacity + 1];
-		N = 0;
+	private Key[] pq;
+	private int N;
+
+	/* Constructor */
+	public _20MaximumPriorityQueue(){
+		// Create array of capacity + 1 as index 1 is 1st element
+		this.pq = (Key[]) new Comparable[1+1];
+		this.N = 0;
 	}
 
-	/**
-	 * Initializes an empty priority queue.
-	 */
-	public _20MaximumPriorityQueue() {
-		this(1);
-	}
-
-	/**
-	 * Initializes an empty priority queue with the given initial capacity,
-	 * using the given comparator.
-	 * @param initCapacity the initial capacity of the priority queue
-	 * @param comparator the order in which to compare the keys
-	 */
-	public _20MaximumPriorityQueue(int initCapacity, Comparator<Key> comparator) {
-		this.comparator = comparator;
-		pq = (Key[]) new Object[initCapacity + 1];
-		N = 0;
-	}
-
-	/**
-	 * Initializes an empty priority queue using the given comparator.
-	 * @param comparator the order in which to compare the keys
-	 */
-	public _20MaximumPriorityQueue(Comparator<Key> comparator) {
-		this(1, comparator);
-	}
-
-	/**
-	 * Initializes a priority queue from the array of keys.
-	 * Takes time proportional to the number of keys, using sink-based heap construction.
-	 * @param keys the array of keys
-	 */
-	public _20MaximumPriorityQueue(Key[] keys) {
-		N = keys.length;
-		pq = (Key[]) new Object[keys.length + 1]; 
-		for (int i = 0; i < N; i++)
-			pq[i+1] = keys[i];
-		for (int k = N/2; k >= 1; k--)
-			sink(k);
-		assert isMaxHeap();
-	}
-
-	/**
-	 * Is the priority queue empty?
-	 * @return true if the priority queue is empty; false otherwise
-	 */
-	public boolean isEmpty() {
+	public boolean isEmpty(){
 		return N == 0;
 	}
 
-	/**
-	 * Returns the number of keys on the priority queue.
-	 * @return the number of keys on the priority queue
-	 */
-	public int size() {
-		return N;
+	/* Insert into the Priority Queue */
+	public void insert(Key x){
+		// O(lgN)
+		if (++N == pq.length)	resize(N*2);
+		// Step 1: Insert at end
+		pq[N] = x;
+		// Step 2: Promote to appropriate position
+		swim(N);
 	}
 
-	/**
-	 * Returns a largest key on the priority queue.
-	 * @return a largest key on the priority queue
-	 * @throws java.util.NoSuchElementException if the priority queue is empty
-	 */
-	public Key max() {
-		if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+	/* Insertion helper */
+	private void swim(int index){
+		// O(lgN)
+		while (index > 1 && less(index/2, index)){
+			exch(index, index/2);
+			index = index / 2;
+		}
+	}
+
+	/* Deletion of max element from PQ */
+	public Key delMax(){
+		// O(lgN)
+		if (isEmpty())	throw new NoSuchElementException("Empty Priority Queue.");
+		// Step 1: Swap pq[1] with pq[N]
+		exch(1, N);
+		// Step 2: Resize pq
+		Key max = pq[N--];
+		pq[N+1] = null;	// prevent loitering
+		if (N + 1 == pq.length/4)	resize(pq.length/2);
+		// Step 3: Demote pq[1] to correct position
+		sink(1);
+		// Step 4: Return max element
+		return max;
+	}
+
+	// Deletion helper 
+	private void sink(int index){
+		// O(lgN)
+		// look at children, swap with larger one
+		while (2*index <= N){
+			int j = 2*index;
+			if (j < N && less(j, j+1))	j++;
+			if (!less(index, j))	break;
+			exch(index, j);
+			index = j;
+		}
+	}
+
+
+	public Key max(){
+		// O(1)
+		if (isEmpty())	throw new NoSuchElementException("Empty Priority Queue.");
 		return pq[1];
 	}
 
-	// helper function to double the size of the heap array
-	private void resize(int capacity) {
-		assert capacity > N;
-		Key[] temp = (Key[]) new Object[capacity];
-		for (int i = 1; i <= N; i++) temp[i] = pq[i];
-		pq = temp;
+	private boolean less(int i, int j){
+		//System.out.println("Comparing: " + i + " with " + j + ". Length: " + pq.length + ". N = " + N);
+		return pq[i].compareTo(pq[j]) < 0;
 	}
 
-	/**
-	 * Adds a new key to the priority queue.
-	 * @param x the new key to add to the priority queue
-	 */
-	public void insert(Key x) {
-
-		// double size of array if necessary
-		if (N >= pq.length - 1) resize(2 * pq.length);
-
-		// add x, and percolate it up to maintain heap invariant
-		pq[++N] = x;
-		swim(N);
-		assert isMaxHeap();
-	}
-
-	/**
-	 * Removes and returns a largest key on the priority queue.
-	 * @return a largest key on the priority queue
-	 * @throws java.util.NoSuchElementException if priority queue is empty.
-	 */
-	public Key delMax() {
-		if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
-		Key max = pq[1];
-		exch(1, N--);
-		sink(1);
-		pq[N+1] = null;		// to avoid loiterig and help with garbage collection
-		if ((N > 0) && (N == (pq.length - 1) / 4)) resize(pq.length / 2);
-		assert isMaxHeap();
-		return max;
-	}
-	
-	/***********************************************************************
-	 * Helper functions to restore the heap invariant.
-	 **********************************************************************/
-
-	private void swim(int k) {
-	while (k > 1 && less(k/2, k)) {
-		exch(k, k/2);
-		k = k/2;
-		}
-	}
-
-	private void sink(int k) {
-		while (2*k <= N) {
-			int j = 2*k;
-			if (j < N && less(j, j+1)) j++;
-			if (!less(k, j)) break;
-			exch(k, j);
-			k = j;
-		}
-	}
-
-	/***********************************************************************
-	 * Helper functions for compares and swaps.
-	 **********************************************************************/
-	private boolean less(int i, int j) {
-		if (comparator == null) {
-			return ((Comparable<Key>) pq[i]).compareTo(pq[j]) < 0;
-		}
-		else {
-			return comparator.compare(pq[i], pq[j]) < 0;
-		}
-	}
-
-	private void exch(int i, int j) {
+	private void exch(int i, int j){
 		Key swap = pq[i];
 		pq[i] = pq[j];
 		pq[j] = swap;
 	}
 
-	// is pq[1..N] a max heap?
-	private boolean isMaxHeap() {
-		return isMaxHeap(1);
+	private void resize(int capacity){
+		Key[] copy = (Key[]) new Comparable[capacity];
+		for (int i = 0; i < N; i++)
+			copy[i] = pq[i];
+		pq = copy; 
 	}
 
-	// is subtree of pq[1..N] rooted at k a max heap?
-	private boolean isMaxHeap(int k) {
-		if (k > N) return true;
-		int left = 2*k, right = 2*k + 1;
-		if (left  <= N && less(k, left))  return false;
-		if (right <= N && less(k, right)) return false;
-		return isMaxHeap(left) && isMaxHeap(right);
-	}
+	//Unit testing 
+	public static void main(String[] args){
+		_20MaximumPriorityQueue<Integer> pq = new _20MaximumPriorityQueue<Integer>();
+		pq.insert(new Integer(1));
+		pq.insert(new Integer(10));
+		pq.insert(new Integer(100));
+		pq.insert(new Integer(-1));
+		pq.insert(new Integer(0));
+		pq.insert(new Integer(-20));
+		pq.insert(new Integer(32));
 
-	/***********************************************************************
- 	 * Iterator
-	 **********************************************************************/
-
-	/**
-	 * Returns an iterator that iterates over the keys on the priority queue
-	 * in descending order.
-	 * The iterator doesn't implement remove() since it's optional.
-	 * @return an iterator that iterates over the keys in descending order
-	 */
-	public Iterator<Key> iterator(){
-		return new HeapIterator();
-	}
-
-	private class HeapIterator implements Iterator<Key>{
-
-		// create a new pq
-		private _20MaximumPriorityQueue<Key> copy;
-
-		// add all items to copy of heap
-		// takes linear time since already in heap order so no keys move
-		public HeapIterator() {
-			if (comparator == null)
-				copy = new _20MaximumPriorityQueue<Key>(size());
-			else
-				copy = new _20MaximumPriorityQueue<Key>(size(), comparator);
-			for (int i = 1; i <= N; i++)
-				copy.insert(pq[i]);
-		}
-
-		public boolean hasNext(){
-			return !copy.isEmpty();
-		}
-		public void remove(){
-			throw new UnsupportedOperationException();
-		}
-		public Key next(){
-			if (!hasNext()) throw new NoSuchElementException();
-			return copy.delMax();
-		}
-	}
-	/**
-	 * Unit tests the MaxPQdata type.
-	 */
-	public static void main(String[] args) {
-		_20MaximumPriorityQueue<String> pq = new _20MaximumPriorityQueue<String>();
-		while (!StdIn.isEmpty()) {
-			String item = StdIn.readString();
-			if (!item.equals("-")) pq.insert(item);
-			else if (!pq.isEmpty()) StdOut.print(pq.delMax() + " ");
-		}
-		StdOut.println("(" + pq.size() + " left on pq)");
+		// Now deletions
+		while (!pq.isEmpty())
+			System.out.print(pq.delMax() + "\t");
+		System.out.println();
 	}
 }
